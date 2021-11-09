a2be994bc3d6287b77845df86ab34c0001dfd650
Vrush-cmd <69395744+Vrush-cmd@users.noreply.github.com>
2021-11-09 15:46:19 +0530

    Update provider.tf

--- Code/provider.tf
+++ Code/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIAXDX2CD6G5LNDNX6Y"
-secret_key = "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"
+access_key = "[AWS_ACCESS_KEY]"
+secret_key = "[AWS_SECRET_KEY]"
 
 }
